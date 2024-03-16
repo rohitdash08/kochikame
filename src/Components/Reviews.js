29482365842ca8552380ab360d83d5668a7c83b6
Rohit Dash <rohitdash08@gmail.com>
2024-03-16 07:03:24 +0530

    Your commit message here

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,46 +1,46 @@
-// import React, { useState } from "react";
-// import { customerReviews } from "../Scripts/reviews";
-// import "../Styles/Reviews.css";
+import React, { useState } from "react";
+import { customerReviews } from "../Scripts/reviews";
+import "../Styles/Reviews.css";
 
-// function Reviews() {
-//   let rMessage, rName, rLocation;
-//   const reviewsLength = customerReviews.length - 1;
-//   const [review, setReview] = useState(0);
+function Reviews() {
+  let rMessage, rName, rLocation;
+  const reviewsLength = customerReviews.length - 1;
+  const [review, setReview] = useState(0);
 
-//   // back to previous review
-//   const backBtnClick = () => {
-//     setReview(review <= 0 ? reviewsLength : review - 1);
-//     handleReviewsUpdation();
-//   };
+  // back to previous review
+  const backBtnClick = () => {
+    setReview(review <= 0 ? reviewsLength : review - 1);
+    handleReviewsUpdation();
+  };
 
-//   // go to newer review
-//   const frontBtnClick = () => {
-//     setReview(review >= reviewsLength ? 0 : review + 1);
-//     handleReviewsUpdation();
-//   };
+  // go to newer review
+  const frontBtnClick = () => {
+    setReview(review >= reviewsLength ? 0 : review + 1);
+    handleReviewsUpdation();
+  };
 
-//   // update reviews
-//   const handleReviewsUpdation = () => {
-//     const reviewMessage = customerReviews[review];
-//     rName = reviewMessage.name;
-//     rLocation = reviewMessage.location;
-//     rMessage = reviewMessage.message;
-//   };
+  // update reviews
+  const handleReviewsUpdation = () => {
+    const reviewMessage = customerReviews[review];
+    rName = reviewMessage.name;
+    rLocation = reviewMessage.location;
+    rMessage = reviewMessage.message;
+  };
 
-//   // list review on visit
-//   handleReviewsUpdation();
+  // list review on visit
+  handleReviewsUpdation();
 
-//   return (
-//     <div className="review-section" id="reviews">
-//       <div className="rw-text-content">
-//         <p className="rw-text-title">
-//           More over <span className="rw-text-num">100+ Customers</span>
-//         </p>
+  return (
+    <div className="review-section" id="reviews">
+      <div className="rw-text-content">
+        <p className="rw-text-title">
+          More over <span className="rw-text-num">100+ Customers</span>
+        </p>
 
-//         <p className="rw-text-desc">Don't believe us, Check clients word</p>
-//       </div>
-//     </div>
-//   );
-// }
+        <p className="rw-text-desc">Don't believe us, Check clients word</p>
+      </div>
+    </div>
+  );
+}
 
-// export default Reviews;
+export default Reviews;
